d4ed8f8f0131e34ceeea7a3dd8ea50d6eeae6b91
João Hugo Leda <74215171+HugoLeda@users.noreply.github.com>
2023-05-20 09:59:15 -0300

    Update 01.py

--- Exercicios-19-05/01.py
+++ Exercicios-19-05/01.py
@@ -1,18 +1,19 @@
-# embaralhar string
-
 from random import sample
 
-def embaralharString(texto, transform=1):
-  embaralhado = sample(texto, len(texto))
-  embaralhado = ''.join(embaralhado)
-  
-  if transform == 1:
-    embaralhado = embaralhado.upper()
-  elif transform == 2:
-    embaralhado = embaralhado.lower()
-  
-  return(embaralhado)
+TRANSFORM_UPPER = 1
+TRANSFORM_LOWER = 2
 
-print(embaralharString('João'))
-print(embaralharString('José', 2))
+def embaralhar_string(texto, transform=TRANSFORM_UPPER):
+  string_embaralhada = ''.join(sample(texto, len(texto)))
   
+  if transform == TRANSFORM_UPPER:
+    string_embaralhada = string_embaralhada.upper()
+  elif transform == TRANSFORM_LOWER:
+    string_embaralhada = string_embaralhada.lower()
+  else:
+    raise ValueError("Opção de transformação inválida.")
+  
+  return string_embaralhada
+
+print(embaralhar_string('João'))
+print(embaralhar_string('José', transform=TRANSFORM_LOWER))
